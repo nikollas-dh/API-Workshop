3c93292e7deb16b230aa47e8a0294362b3765b5f
nikollas-dh <nikollasdheyvis17@gmail.com>
2026-03-30 16:48:57 -0300

    Removendo script do repositório

--- APIWorkshop/APIWorkshop/obj/Debug/net10.0/APIWorkshop.AssemblyInfo.cs
+++ APIWorkshop/APIWorkshop/obj/Debug/net10.0/APIWorkshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIWorkshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349ba18e414f489b0b5e9a967fbe5b688aeda08b")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIWorkshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIWorkshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
